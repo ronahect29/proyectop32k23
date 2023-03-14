bae6b2bf7242180a7e7ad94e855676ec1a06fb8c
NelsonUMG <npinedac3@miumg.edu.gt>
2023-03-14 16:47:31 -0600

    Agregando cambios en el dao usuario

--- Proyectop32k23/src/main/java/Seguridad/Modelo/daoUsuario.java
+++ Proyectop32k23/src/main/java/Seguridad/Modelo/daoUsuario.java
@@ -16,12 +16,12 @@ import java.util.List;
  */
 public class daoUsuario {
 
-    private static final String SQL_SELECT = "SELECT usuid, usunombre, usucontrasena FROM tbl_usuario";
-    private static final String SQL_INSERT = "INSERT INTO tbl_usuario(usunombre, usucontrasena) VALUES(?, ?)";
-    private static final String SQL_UPDATE = "UPDATE tbl_usuario SET usunombre=?, usucontrasena=? WHERE usuid = ?";
+    private static final String SQL_SELECT = "SELECT usuid, usunombre, usucontrasena, usuultimasesion, usuestatus FROM tbl_usuario";
+    private static final String SQL_INSERT = "INSERT INTO tbl_usuario(usunombre, usucontrasena, usuultimasesion, usuestatus) VALUES(?, ?, ?, ?)";
+    private static final String SQL_UPDATE = "UPDATE tbl_usuario SET usunombre=?, usucontrasena=?,  usuultimasesion=?, usuestatus=? WHERE usuid = ?";
     private static final String SQL_DELETE = "DELETE FROM tbl_usuario WHERE usuid=?";
-    private static final String SQL_SELECT_NOMBRE = "SELECT usuid, usunombre, usucontrasena FROM tbl_usuario WHERE usunombre = ?";
-    private static final String SQL_SELECT_ID = "SELECT usuid, usunombre, usucontrasena FROM tbl_usuario WHERE usuid = ?";    
+    private static final String SQL_SELECT_NOMBRE = "SELECT usuid, usunombre, usucontrasena, usuultimasesion, usuestatus FROM tbl_usuario WHERE usunombre = ?";
+    private static final String SQL_SELECT_ID = "SELECT usuid, usunombre, usucontrasena, usuultimasesion, usuestatus FROM tbl_usuario WHERE usuid = ?";     
 
     public List<clsUsuario> consultaUsuarios() {
         Connection conn = null;
@@ -37,10 +37,14 @@ public class daoUsuario {
                 int id = rs.getInt("usuid");
                 String nombre = rs.getString("usunombre");
                 String contrasena = rs.getString("usucontrasena");
+                String sesion = rs.getString("usuultimasesion");
+		String estatus = rs.getString("usuestatus");
                 clsUsuario usuario = new clsUsuario();
                 usuario.setIdUsuario(id);
                 usuario.setNombreUsuario(nombre);
                 usuario.setContrasenaUsuario(contrasena);
+                usuario.setUltimaSesionUsuario(sesion);
+                usuario.setEstatusUsuario(estatus);
                 usuarios.add(usuario);
             }
         } catch (SQLException ex) {
@@ -62,7 +66,8 @@ public class daoUsuario {
             stmt = conn.prepareStatement(SQL_INSERT);
             stmt.setString(1, usuario.getNombreUsuario());
             stmt.setString(2, usuario.getContrasenaUsuario());
-
+            stmt.setString(3, usuario.getUltimaSesionUsuario());
+	    stmt.setString(4, usuario.getEstatusUsuario());
             System.out.println("ejecutando query:" + SQL_INSERT);
             rows = stmt.executeUpdate();
             System.out.println("Registros afectados:" + rows);
@@ -86,8 +91,10 @@ public class daoUsuario {
             stmt = conn.prepareStatement(SQL_UPDATE);
             stmt.setString(1, usuario.getNombreUsuario());
             stmt.setString(2, usuario.getContrasenaUsuario());
-            stmt.setInt(3, usuario.getIdUsuario());
-
+            stmt.setString(3, usuario.getUltimaSesionUsuario());
+	    stmt.setString(4, usuario.getEstatusUsuario());
+            stmt.setInt(5, usuario.getIdUsuario());
+            
             rows = stmt.executeUpdate();
             System.out.println("Registros actualizado:" + rows);
 
@@ -138,11 +145,14 @@ public class daoUsuario {
                 int id = rs.getInt("usuid");
                 String nombre = rs.getString("usunombre");
                 String contrasena = rs.getString("usucontrasena");
-
+                String sesion = rs.getString("usuultimasesion");
+		String estatus = rs.getString("usuestatus");
                 //usuario = new clsUsuario();
                 usuario.setIdUsuario(id);
                 usuario.setNombreUsuario(nombre);
                 usuario.setContrasenaUsuario(contrasena);
+                usuario.setUltimaSesionUsuario(sesion);
+                usuario.setEstatusUsuario(estatus);
                 System.out.println(" registro consultado: " + usuario);                
             }
             //System.out.println("Registros buscado:" + persona);
@@ -172,11 +182,14 @@ public class daoUsuario {
                 int id = rs.getInt("usuid");
                 String nombre = rs.getString("usunombre");
                 String contrasena = rs.getString("usucontrasena");
-
+                String sesion = rs.getString("usuultimasesion");
+		String estatus = rs.getString("usuestatus");
                 //usuario = new clsUsuario();
                 usuario.setIdUsuario(id);
                 usuario.setNombreUsuario(nombre);
                 usuario.setContrasenaUsuario(contrasena);
+                usuario.setUltimaSesionUsuario(sesion);
+                usuario.setEstatusUsuario(estatus);
                 System.out.println(" registro consultado: " + usuario);                
             }
             //System.out.println("Registros buscado:" + persona);
