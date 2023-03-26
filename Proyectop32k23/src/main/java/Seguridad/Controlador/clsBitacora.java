c40143b6004bd549a0c4b3413104cc4f7899e4b4
CarlosGonzalez20 <ll3224645@gmail.com>
2023-03-26 15:57:09 -0600

    eliminando código de más en clsBitácora (código innecesario)

--- Proyectop32k23/src/main/java/Seguridad/Controlador/clsBitacora.java
+++ Proyectop32k23/src/main/java/Seguridad/Controlador/clsBitacora.java
@@ -109,9 +109,6 @@ public class clsBitacora {
         daoBitacora daobitacora = new daoBitacora();
         return daobitacora.insert(codigoUsuario, codigoAplicacion, accion);
     }
-    public int setIngresaUsuarioBitacora(int codigoUsuario, int cdofigoAplicacion, String accion){
-        
-    }
     public List<clsBitacora> getListadoBitacora(String primeraFecha, String segundaFecha) throws ParseException
     {
         daoBitacora daobitacora = new daoBitacora();
