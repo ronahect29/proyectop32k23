4fe1bb31ff6cfc2744c24bdf9f6cdbaadf7bb201
sofia Tobar <kgomezt2@miumg.edu.gt>
2023-03-26 15:25:45 -0600

    se agrego bitacora al boton modificar

--- Proyectop32k23/src/main/java/Seguridad/Vista/frmMantenimientoUsuario.java
+++ Proyectop32k23/src/main/java/Seguridad/Vista/frmMantenimientoUsuario.java
@@ -481,6 +481,11 @@ int codigoAplicacion=110;
         JOptionPane.showMessageDialog(null, "Registro Modificado\n", 
                     "Información del Sistema", JOptionPane.INFORMATION_MESSAGE);        
         llenadoDeTablas();
+        
+        int resultadoBitacora=0;
+        clsBitacora bitacoraRegistro = new clsBitacora();
+        resultadoBitacora = bitacoraRegistro.setIngresarBitacora(usuario.getIdUsuario(), codigoAplicacion, "UPD");
+        
         limpiarTextos();
     }//GEN-LAST:event_btnModificarActionPerformed
 
