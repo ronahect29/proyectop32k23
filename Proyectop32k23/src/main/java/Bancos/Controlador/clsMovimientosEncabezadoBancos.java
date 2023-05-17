3cd78c1252bb78f3a18673396d6eea5502bec213
sofia Tobar <kgomezt2@miumg.edu.gt>
2023-05-17 15:11:05 -0600

    variables agregadas al cls

---
+++ Proyectop32k23/src/main/java/Bancos/Controlador/clsMovimientosEncabezadoBancos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Hecho por: Karla Sofia Góomez Tobar 9959-21-1896
+package Bancos.Controlador;
+import java.util.List;
+import Bancos.Modelo.daoMovimientosEncabezadoBancos;
+/**
+ *
+ * @author visitante
+ */
+public class clsMovimientosEncabezadoBancos {
+    private int movId;
+    private int codBanco;
+    private int cueId;
+    private int tipMovId;
+    private String movFecha;
+    private int cueEmId;
