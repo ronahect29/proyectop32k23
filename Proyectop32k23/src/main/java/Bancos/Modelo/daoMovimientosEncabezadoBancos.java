69c6fcb2d37f6d930eaaa1297e502af5756418ab
sofia Tobar <kgomezt2@miumg.edu.gt>
2023-05-17 15:19:45 -0600

    agregado consultar movimiento encabezado

--- Proyectop32k23/src/main/java/Bancos/Modelo/daoMovimientosEncabezadoBancos.java
+++ Proyectop32k23/src/main/java/Bancos/Modelo/daoMovimientosEncabezadoBancos.java
@@ -27,3 +27,50 @@ public class daoMovimientosEncabezadoBancos {
     private static final String SQL_SELECT_ID = "SELECT codBanco, cueId, tipMovId, movFecha, cueEmId  FROM tbl_movimientosEncabezadoBancos WHERE movId = ?";     
 
 
+
+    public String fechaActual() {
+
+        java.util.Date fecha = new java.util.Date();
+        //SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/YYYY");
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("YYYY/MM/dd HH:mm:ss");
+
+        return formatoFecha.format(fecha);
+
+    }
+
+   public List<clsMovimientosEncabezadoBancos> consultaMovimientosEncabezado() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        
+        List<clsMovimientosEncabezadoBancos> MovEncabezadoB = new ArrayList<>();
+        try {
+            conn = Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("movId");
+                int codigo = rs.getInt("codBanco");
+                int idcuenta = rs.getInt("cueId");
+                int idTipoMonv = rs.getInt("tipMovId");
+                String fecha = rs.getString("movFecha");
+                int idCuentaEm = rs.getInt("cueEmId");
+                clsMovimientosEncabezadoBancos MovEncabezado = new clsMovimientosEncabezadoBancos();
+                MovEncabezado.setMovId(id);
+                MovEncabezado.setCodBanco(codigo);
+                MovEncabezado.setCueId(idcuenta);
+                MovEncabezado.setTipMovId(idTipoMonv);
+                MovEncabezado.setMovFecha(fecha);
+                MovEncabezado.setCueEmId(idCuentaEm);
+                MovEncabezadoB.add(MovEncabezado);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        } finally {
+            Conexion.close(rs);
+            Conexion.close(stmt);
+            Conexion.close(conn);
+        }
+        return MovEncabezadoB;
+    }
+
