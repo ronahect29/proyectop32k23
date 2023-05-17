3d6fd4921198875a9de5b9ff6663d22dd8a0e376
Esduardo Del Aguila <daguilae@yahoo.com>
2023-05-17 11:55:57 -0600

    agregando cambio conexion

--- Proyectop32k23/src/main/java/Seguridad/Modelo/Conexion.java
+++ Proyectop32k23/src/main/java/Seguridad/Modelo/Conexion.java
@@ -4,14 +4,9 @@ import java.sql.*;
 
 public class Conexion {
 private static final String JDBC_URL = "jdbc:mysql://localhost/proyectop312023?useSSL=false&serverTimezone=UTC";    
-<<<<<<< HEAD
     private static final String JDBC_USER = "usuprueba";
     private static final String JDBC_PASS = "123456";
-=======
-    private static final String JDBC_USER = "root";
-    private static final String JDBC_PASS = "";
->>>>>>> aaa3eeb472ea989247198aa0bcc23bec2e990b92
-   
+  
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
     }
