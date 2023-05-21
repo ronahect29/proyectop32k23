c6420d7cfcb16ac866c4fe5b3e42f722ebf525ad
Victor Jerez <vjerezm@miumg.edu.gt>
2023-05-20 22:05:19 -0600

    Se corrige la conexion

--- Proyectop32k23/src/main/java/Seguridad/Controlador/clsConexion.java
+++ Proyectop32k23/src/main/java/Seguridad/Controlador/clsConexion.java
@@ -5,10 +5,10 @@ import java.sql.*;
 public class clsConexion {
 
 
-  private static final String JDBC_URL = "jdbc:mysql://localhost/pruebaproyecto?useSSL=false&serverTimezone=UTC";  
+  private static final String JDBC_URL = "jdbc:mysql://localhost/proyectop312023?useSSL=false&serverTimezone=UTC";  
 
-    private static final String JDBC_USER = "root";
-    private static final String JDBC_PASS = "VictorJJerez*33";
+    private static final String JDBC_USER = "UsuPrueba";
+    private static final String JDBC_PASS = "123456";
 
    
     public static Connection getConnection() throws SQLException {
